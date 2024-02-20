0eff0bf9a08f6e735b49805b4542a47f35ddc91a
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-02-20 06:42:21 +0100

    Add create album button on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Mes Albums') }}
         </h2>
+        <a href="{{ route('album.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+            Créer
+        </a>
     </x-slot>
 
     <div class="py-12">
@@ -15,7 +18,9 @@
                             <p>{{ count($album->photos) }}</p>
                         @endforeach
                     @else 
-                        {{ __('Vous n\'avez aucun album pour le moment.') }}
+                        <div class="text-center">
+                            {{ __('Vous n\'avez aucun album pour le moment.') }}
+                        </div>
                     @endif
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/', function () {
 Route::get('/dashboard', [DashboardController::class, 'list'])
     ->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/create-album', [DashboardController::class, 'list'])
+    ->middleware(['auth', 'verified'])->name('album.create');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
