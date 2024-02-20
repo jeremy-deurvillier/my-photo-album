97a319af223bbe09f77436ec385c5a5ad2af0905
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-02-20 03:16:31 +0100

    Add albums and photos migrations

---
+++ database/migrations/2024_02_20_020842_create_photos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 40);
+            $table->string('hash', 64);
+            $table->timestamps();
+            $table->unsignedBigInteger('album_id');
+            $table->foreign('album_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('photos');
+    }
+};
