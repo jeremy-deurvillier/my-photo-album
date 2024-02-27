7a147070fd7fed16ef118bec89934fb2f251c3a5
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-02-27 15:51:04 +0100

    Update photo items and add buttons components for photos list on a album

---
+++ resources/views/components/photo-button.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'flex items-center px-2 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-full font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</a>
--- resources/views/single-album.blade.php
+++ resources/views/single-album.blade.php
@@ -123,18 +123,30 @@
                 x-data=""
                 x-on:click.prevent="$dispatch('open-modal', 'add-photos')"
             >
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                    <title>add</title>
+                    <path d="M18.984 12.984h-6v6h-1.969v-6h-6v-1.969h6v-6h1.969v6h6v1.969z"></path>
+                </svg>
                 {{ __('Ajouter') }}
             </x-primary-button>
             <x-secondary-button 
                 x-data=""
                 x-on:click.prevent="$dispatch('open-modal', 'update-album')"
             >
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                    <title>edit</title>
+                    <path d="M20.719 7.031l-1.828 1.828-3.75-3.75 1.828-1.828q0.281-0.281 0.703-0.281t0.703 0.281l2.344 2.344q0.281 0.281 0.281 0.703t-0.281 0.703zM3 17.25l11.063-11.063 3.75 3.75-11.063 11.063h-3.75v-3.75z"></path>
+                </svg>
                 {{ __('Changer le titre') }}
             </x-secondary-button>
             <x-danger-button 
                 x-data=""
                 x-on:click.prevent="$dispatch('open-modal', 'confirm-album-deletion')"
             >
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                    <title>delete</title>
+                    <path d="M15.516 3.984h3.469v2.016h-13.969v-2.016h3.469l1.031-0.984h4.969zM8.438 11.859l2.156 2.156-2.109 2.109 1.406 1.406 2.109-2.109 2.109 2.109 1.406-1.406-2.109-2.109 2.109-2.156-1.406-1.406-2.109 2.156-2.109-2.156zM6 18.984v-12h12v12q0 0.797-0.609 1.406t-1.406 0.609h-7.969q-0.797 0-1.406-0.609t-0.609-1.406z"></path>
+                </svg>
                 {{ __('Supprimer') }}
             </x-danger-button>
         </div>
@@ -152,9 +164,34 @@
                                     $extension = '.' . $explodeFileName[count($explodeFileName) - 1];
                                     $bg = 'background: url("' . asset('uploads/' . $photo->hash . $extension) . '") no-repeat center center';
                                 @endphp
-                                <a href="{{ route('file.show', ['photo' => $photo->id]) }}" target="_blank" class="photo-link">
-                                    <div style="{{ $bg }}"></div>
-                                </a>
+                                <div class="photo-link overflow-hidden">
+                                    <div style="{{ $bg }}">
+                                        <div class="buttons-group flex flex-col items-center justify-between w-20 h-full overflow-hidden p-2">
+                                            <div class="grid">
+                                                <x-photo-button href="{{ route('file.show', ['photo' => $photo->id]) }}" target="_blank">
+                                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                                                        <title>open</title>
+                                                        <path d="M21 11.016v-8.016h-8.016l3.328 3.281-10.031 10.031-3.281-3.328v8.016h8.016l-3.328-3.281 10.031-10.031z"></path>
+                                                    </svg>
+                                                </x-photo-button>
+                                                <x-photo-button>
+                                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                                        <title>share</title>
+                                                        <path d="M18 16.078q1.219 0 2.063 0.867t0.844 2.039q0 1.219-0.867 2.086t-2.039 0.867-2.039-0.867-0.867-2.086q0-0.469 0.047-0.656l-7.078-4.125q-0.891 0.797-2.063 0.797-1.219 0-2.109-0.891t-0.891-2.109 0.891-2.109 2.109-0.891q1.172 0 2.063 0.797l7.031-4.078q-0.094-0.469-0.094-0.703 0-1.219 0.891-2.109t2.109-0.891 2.109 0.891 0.891 2.109-0.891 2.109-2.109 0.891q-1.125 0-2.063-0.844l-7.031 4.125q0.094 0.469 0.094 0.703t-0.094 0.703l7.125 4.125q0.844-0.75 1.969-0.75z"></path>
+                                                    </svg>
+                                                </x-photo-button>
+                                            </div>
+                                            <div class="grid">
+                                                <x-photo-danger-button>
+                                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                                                        <title>delete</title>
+                                                        <path d="M15.516 3.984h3.469v2.016h-13.969v-2.016h3.469l1.031-0.984h4.969zM8.438 11.859l2.156 2.156-2.109 2.109 1.406 1.406 2.109-2.109 2.109 2.109 1.406-1.406-2.109-2.109 2.109-2.156-1.406-1.406-2.109 2.156-2.109-2.156zM6 18.984v-12h12v12q0 0.797-0.609 1.406t-1.406 0.609h-7.969q-0.797 0-1.406-0.609t-0.609-1.406z"></path>
+                                                    </svg>
+                                                </x-photo-danger-button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         @else 
                             <div class="text-center">
